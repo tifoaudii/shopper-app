53d22c509a89a543a755cf9b91292dd6938358d8
tifoaudy <tifofilkomub@gmail.com>
2019-01-03 09:34:02 +0700

    add input accesory view and IBDesignable

--- shopper/Controller/MainVC.swift
+++ shopper/Controller/MainVC.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTextField: CurrencyTextField!
+    @IBOutlet weak var priceTextField: CurrencyTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calculateBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calculateBtn.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        calculateBtn.setTitle("Calculate", for: .normal)
+        calculateBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calculateBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTextField.inputAccessoryView = calculateBtn
+        priceTextField.inputAccessoryView = calculateBtn
     }
-
+    
+    @objc func calculate(){
+        print("clicked")
+    }
 
 }
 
--- shopper/Views/CurrencyTextField.swift
+++ shopper/Views/CurrencyTextField.swift
@@ -8,16 +8,33 @@
 
 import UIKit
 
+//add class atributte to show changes in interface builder
+@IBDesignable
 class CurrencyTextField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        //called after there are any logic changes happen in this class
+        customeView()
+    }
 
-    //lifecycle method, called after loaded
+    //lifecycle method, called after the textfield loaded
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        customeView()
+    }
+    
+    private func customeView(){
+        //custom textfield
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 10
         textAlignment = .center
         
+        //check the value of placeholder
+        //if placeholder not nill, then it assigned to p
         if let p = placeholder {
+            
+            //placeholder not nil
             let place = NSAttributedString(string: p, attributes:[.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
             attributedPlaceholder = place
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
